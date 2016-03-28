0129ab75a3a82382bc55ad8b10b5308967da0ebf
Oddur Carl Thorarensen <oddurc@gmail.com>
2016-03-28 19:15:38 +0000

    FCFS working without statistics
    Statistics not working correctly

---
+++ ProcessScheduling_GeirEggertsson_GuðmundurRafnGylfason_OddurCarlThorarensen/src/com/ru/usty/scheduling/ScheduleData.java
@@ -0,0 +1,17 @@
+package com.ru.usty.scheduling;
+
+public class ScheduleData {
+	
+	public long TAT;
+	public long WT;
+	public int processCount;
+
+	public ScheduleData() {
+		this.TAT = 0;
+		this.WT = 0;
+		this.processCount = 0;
+	}
+	
+	
+	
+}
--- ProcessScheduling_GeirEggertsson_GuðmundurRafnGylfason_OddurCarlThorarensen/src/com/ru/usty/scheduling/Scheduler.java
+++ ProcessScheduling_GeirEggertsson_GuðmundurRafnGylfason_OddurCarlThorarensen/src/com/ru/usty/scheduling/Scheduler.java
@@ -1,12 +1,29 @@
 package com.ru.usty.scheduling;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import com.ru.usty.scheduling.process.ProcessExecution;
+import com.ru.usty.scheduling.process.ProcessInfo;
+import com.ru.usty.scheduling.process.ProcessHandler;
 
 public class Scheduler {
 
 	ProcessExecution processExecution;
+	ProcessInfo processInfo;
+	ProcessHandler processHandler;
 	Policy policy;
 	int quantum;
+	
+	ScheduleData SD_FCFS;
+	
+	int nextProcess;
+	
+	long schStart, schFinish;
+	
+	boolean processRunning = false;
+
+	Queue<Integer> processQueue;
 
 	/**
 	 * Add any objects and variables here (if needed)
@@ -31,19 +48,23 @@ public class Scheduler {
 
 		this.policy = policy;
 		this.quantum = quantum;
-
-		/**
-		 * Add general initialization code here (if needed)
-		 */
+		
+		//this.schStart = System.currentTimeMillis(); //sets the start time of this scheduler
 
 		switch(policy) {
 		case FCFS:	//First-come-first-served
+			processQueue = new LinkedList<Integer>();
+			SD_FCFS = new ScheduleData();
+			
 			System.out.println("Starting new scheduling task: First-come-first-served");
 			/**
 			 * Add your policy specific initialization code here (if needed)
 			 */
 			break;
 		case RR:	//Round robin
+			System.out.println("WT: " + (SD_FCFS.WT/SD_FCFS.processCount) + " - TAT: " + (SD_FCFS.TAT/SD_FCFS.processCount) );
+			processQueue = new LinkedList<Integer>();
+			
 			System.out.println("Starting new scheduling task: Round robin, quantum = " + quantum);
 			/**
 			 * Add your policy specific initialization code here (if needed)
@@ -78,7 +99,6 @@ public class Scheduler {
 		/**
 		 * Add general scheduling or initialization code here (if needed)
 		 */
-
 	}
 
 	/**
@@ -86,10 +106,52 @@ public class Scheduler {
 	 */
 	public void processAdded(int processID) {
 
-		/**
-		 * Add scheduling code here
-		 */
+		this.policy = policy;
+		this.quantum = quantum;
 
+		switch(policy) {
+		case FCFS:	//First-come-first-served
+			processQueue.add(processID);
+			processInfo = processExecution.getProcessInfo(processID);
+			if(processRunning == false){
+				processExecution.switchToProcess(processID);
+				processRunning = true;
+			}
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case RR:	//Round robin
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case SPN:	//Shortest process next
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case SRT:	//Shortest remaining time
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case HRRN:	//Highest response ratio next
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case FB:	//Feedback
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		}
 	}
 
 	/**
@@ -97,9 +159,61 @@ public class Scheduler {
 	 */
 	public void processFinished(int processID) {
 
-		/**
-		 * Add scheduling code here
-		 */
+		this.policy = policy;
+		this.quantum = quantum;
 
+		switch(policy) {
+		case FCFS:	//First-come-first-served
+				
+			processQueue.remove();
+			
+			//processHandler = new ProcessHandler();
+			//processInfo = processExecution.getProcessInfo(processID); 
+			
+			SD_FCFS.WT = SD_FCFS.WT + processInfo.elapsedWaitingTime;
+			SD_FCFS.TAT = SD_FCFS.TAT + processInfo.elapsedExecutionTime;
+			SD_FCFS.processCount++;
+//			System.out.println(processInfo.);
+			
+			processRunning = false;
+			
+			if (!processQueue.isEmpty()){
+				System.out.println(processInfo.elapsedWaitingTime);
+				processExecution.switchToProcess(processQueue.element()); 
+				processRunning = true;
+			}
+			
+			break;
+		case RR:	//Round robin
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case SPN:	//Shortest process next
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case SRT:	//Shortest remaining time
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case HRRN:	//Highest response ratio next
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		case FB:	//Feedback
+			
+			/**
+			 * Add your policy specific initialization code here (if needed)
+			 */
+			break;
+		}
 	}
 }
