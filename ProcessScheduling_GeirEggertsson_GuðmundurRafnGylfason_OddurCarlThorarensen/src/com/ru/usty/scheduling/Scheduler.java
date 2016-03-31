47f90dd736186b56daba3a9dd2a0c71a8eafcd69
geireggerts <geire89@gmail.com>
2016-03-31 22:09:50 +0000

    statistics bætt við

--- ProcessScheduling_GeirEggertsson_GuðmundurRafnGylfason_OddurCarlThorarensen/src/com/ru/usty/scheduling/Scheduler.java
+++ ProcessScheduling_GeirEggertsson_GuðmundurRafnGylfason_OddurCarlThorarensen/src/com/ru/usty/scheduling/Scheduler.java
@@ -7,17 +7,23 @@ import com.ru.usty.scheduling.process.ProcessExecution;
 import com.ru.usty.scheduling.process.ProcessInfo;
 import com.ru.usty.scheduling.process.ProcessHandler;
 
-public class Scheduler {
+public class Scheduler implements Runnable {
 
-	public ProcessExecution processExecution;
+	ProcessExecution processExecution;
 	ProcessInfo processInfo;
 	ProcessInfo runningProcessInfo;
 	int runningProcessID;
+	int compareID_1;
+	int compareID_2;
+	
+	boolean incIndex;
 	
 	ProcessHandler processHandler;
 	Policy policy;
 	int quantum;
+	int quantumTime;
 	int RR_Index;
+	int currentRunningIndex;
 	
 	ScheduleData SD_FCFS;
 	
@@ -33,6 +39,24 @@ public class Scheduler {
 	Queue<Integer> processQueue;
 	LinkedList<Integer> processList;
 	PriorityQueue<ProcessData> prioQ;
+	
+	long[] arriving = new long[15];
+	long[] finished = new long[15];
+	long[] starting = new long[15];
+	int processCount = 0;
+	long sumResponseTime = 0;
+	long sumTurnaroundTime= 0;
+	long avgResponseTime = 0;
+	long avgTurnaroundTime = 0;
+	
+	
+	//------------------------------------------------------------
+	boolean oneThreadRR = true;
+	Thread threadRR;
+	long startedProcess; 
+	boolean noProcessRunning = true;
+	int procID;
+
 
 	/**
 	 * Add any objects and variables here (if needed)
@@ -58,6 +82,19 @@ public class Scheduler {
 		this.policy = policy;
 		this.quantum = quantum;
 		
+		// Initializing for calculating purposes
+		for(int i = 0; i <= 14; i++){
+			arriving[i] = 0;
+			finished[i] = 0;
+			starting[i] = 0;	
+		}
+		
+		processCount = 0;
+		sumResponseTime = 0;
+		sumTurnaroundTime= 0;
+		avgResponseTime = 0;
+		avgTurnaroundTime = 0;
+		
 		//this.schStart = System.currentTimeMillis(); //sets the start time of this scheduler
 
 		switch(policy) {
@@ -72,11 +109,20 @@ public class Scheduler {
 			break;
 		case RR:	//Round robin
 			//System.out.println("WT: " + (SD_FCFS.WT/SD_FCFS.processCount) + " - TAT: " + (SD_FCFS.TAT/SD_FCFS.processCount) );
-
-			RR_Index = 0;
-			processList = new LinkedList<Integer>();
-			
 			System.out.println("Starting new scheduling task: Round robin, quantum = " + quantum);
+//			RR_Index = 0;
+//			processList = new LinkedList<Integer>();
+//			this.quantumTime = quantum;
+//			processRunning = false;
+//			compareID_2 = -1;
+//			incIndex = true;
+			
+//			if(oneThreadRR){
+//				threadRR = new Thread(this);
+//				threadRR.start();
+//			}
+//			oneThreadRR = false;
+
 			/**
 			 * Add your policy specific initialization code here (if needed)
 			 */
@@ -133,6 +179,8 @@ public class Scheduler {
 
 		this.policy = policy;
 		this.quantum = quantum;
+		
+		arriving[processID] = System.currentTimeMillis();
 
 		switch(policy) {
 		case FCFS:	//First-come-first-served
@@ -140,15 +188,38 @@ public class Scheduler {
 			processInfo = processExecution.getProcessInfo(processID);
 			if(processRunning == false){
 				processExecution.switchToProcess(processID);
+				starting[processID] = System.currentTimeMillis();
 				processRunning = true;
 			}
 			break;
 		case RR:	//Round robin
-
-			processList.add(processID);
+//			processList.add(processID);
+//			
+//			if (incIndex) {
+//				if ((RR_Index + 1) >= processList.size() ) {
+//					RR_Index = 0;
+//				}
+//				else {
+//					RR_Index++;
+//				}
+//			}
+//			runProcess(RR_Index);
+//			
+//			processList.add(processID);
+//
+//			if(noProcessRunning == true){
+//				
+//				//procID = processList.remove();
+//				procID = processList.getFirst();
+//				startedProcess = System.currentTimeMillis();
+//				processExecution.switchToProcess(procID); 
+//				noProcessRunning = false;
+//				
+////				if(starting[processID] == 0){
+////					starting[processID] = System.currentTimeMillis();
+////				}
+//			}
 			
-			RR_SwitchToProcess(processID);	
-
 			/**
 			 * Add your policy specific initialization code here (if needed)
 			 */
@@ -160,6 +231,7 @@ public class Scheduler {
 				int ID = prioQ.element().processID;
 				runningProcessInfo = processExecution.getProcessInfo(ID);
 				processExecution.switchToProcess(ID);
+				starting[processID] = System.currentTimeMillis();
 				processRunning = true;
 			}
 			
@@ -175,6 +247,9 @@ public class Scheduler {
 				runningProcessInfo = processExecution.getProcessInfo(ID);
 				runningProcessID = ID;			
 				processExecution.switchToProcess(ID);
+				if (starting[ID] == 0) {
+					starting[ID] = System.currentTimeMillis();
+				}
 				processRunning = true;
 			}
 			else {
@@ -182,8 +257,11 @@ public class Scheduler {
 				long remTime = pi.totalServiceTime - pi.elapsedExecutionTime;
 				
 				if (remTime > processInfo.totalServiceTime) { // Ef remaining time á nýja process er minni en á núverandi process
-					runningProcessInfo = processExecution.getProcessInfo(processID); // Þá skiptum við yfir á nýja
+					runningProcessInfo = processExecution.getProcessInfo(processID); // Þá skiptum við yfir á nýja process-inn
 					processExecution.switchToProcess(processID);
+					if (starting[processID] == 0) {
+						starting[processID] = System.currentTimeMillis();
+					}
 					processRunning = true;
 					
 					// Vil þá taka gamla processinn úr queue og setja hann aftur inn með annan runtime (þeas núverandi remaining time)
@@ -223,6 +301,9 @@ public class Scheduler {
 			runningProcessInfo = processExecution.getProcessInfo(ID);
 			runningProcessID = ID;			
 			processExecution.switchToProcess(ID);
+			if (starting[ID] == 0) {
+				starting[ID] = System.currentTimeMillis();
+			}
 			processRunning = true;	
 			
 			
@@ -243,6 +324,9 @@ public class Scheduler {
 	 * DO NOT CHANGE DEFINITION OF OPERATION
 	 */
 	public void processFinished(int processID) {
+		
+		processCount++;
+		finished[processID] = System.currentTimeMillis();
 
 		this.policy = policy;
 		this.quantum = quantum;
@@ -255,28 +339,52 @@ public class Scheduler {
 			//processHandler = new ProcessHandler();
 			//processInfo = processExecution.getProcessInfo(lastProcess); 
 			
-			SD_FCFS.WT = SD_FCFS.WT + processInfo.elapsedWaitingTime;
-			SD_FCFS.TAT = SD_FCFS.TAT + processInfo.elapsedExecutionTime;
-			SD_FCFS.processCount++;
+//			SD_FCFS.WT = SD_FCFS.WT + processInfo.elapsedWaitingTime;
+//			SD_FCFS.TAT = SD_FCFS.TAT + processInfo.elapsedExecutionTime;
+//			SD_FCFS.processCount++;
 //			System.out.println(processInfo.);
 			
 			processRunning = false;
 			
 			if (!processQueue.isEmpty()){
-				System.out.println(processInfo.elapsedWaitingTime);
-				processExecution.switchToProcess(processQueue.element()); 
+				//System.out.println(processInfo.elapsedWaitingTime);
+				int ID = processQueue.element();
+				processExecution.switchToProcess(ID);
+				starting[ID] = System.currentTimeMillis();
 				processRunning = true;
+			}
+			
+			if(processCount == 15){
+				statistics();
 			}
 			
 			break;
 		case RR:	//Round robin
-			processList.remove(processList.indexOf(processID));
+//			processList.remove(runningProcessID);
+//			processRunning = false;
+//			
+//			if(!processList.isEmpty()) {
+//				if (RR_Index >= processList.size() ) {
+//					RR_Index = 0;
+//				}
+//				
+//				runProcess(RR_Index);
+//			}
+//			
 			
-			processRunning = false;
-			
-			if(!processList.isEmpty()) {
-				RR_SwitchToProcess(processID);
-			}
+//			if(!processList.isEmpty()){
+//				
+//				procID = processList.remove();
+//				startedProcess = System.currentTimeMillis();
+//				processExecution.switchToProcess(procID);
+				
+//				if(starting[procID] == 0){
+//					starting[procID] = System.currentTimeMillis();
+//				}
+//			}
+//			else{
+//				noProcessRunning = true;
+//			}
 			
 			/**
 			 * Add your policy specific initialization code here (if needed)
@@ -292,7 +400,12 @@ public class Scheduler {
 				runningProcessInfo = processExecution.getProcessInfo(ID);
 				runningProcessID = ID;
 				processExecution.switchToProcess(ID);
+				starting[ID] = System.currentTimeMillis();
 				processRunning = true;
+			}
+			
+			if(processCount == 15){
+				statistics();
 			}
 			/**
 			 * Add your policy specific initialization code here (if needed)
@@ -306,7 +419,14 @@ public class Scheduler {
 				runningProcessInfo = processExecution.getProcessInfo(ID);
 				runningProcessID = ID;
 				processExecution.switchToProcess(ID);
+				if (starting[ID] == 0) {
+					starting[ID] = System.currentTimeMillis();
+				}
 				processRunning = true;
+			}
+			
+			if(processCount == 15){
+				statistics();
 			}
 			
 			/**
@@ -321,7 +441,14 @@ public class Scheduler {
 				runningProcessInfo = processExecution.getProcessInfo(ID);
 				runningProcessID = ID;
 				processExecution.switchToProcess(ID);
+				if (starting[ID] == 0) {
+					starting[ID] = System.currentTimeMillis();
+				}
 				processRunning = true;
+			}
+			
+			if(processCount == 15){
+				statistics();
 			}
 			
 			/**
@@ -371,6 +498,121 @@ public class Scheduler {
 			 }
 		};
 		t.start();		
+	}
+	
+	public void runProcess(int index) {
+		if (!processList.isEmpty()) {
+			
+			//long startTime = System.currentTimeMillis();
+			runningProcessID = processList.get(RR_Index);
+			compareID_1 = processList.get(RR_Index);
+			
+			if (compareID_1 != compareID_2) {
+				processExecution.switchToProcess(runningProcessID);
+			}
+//			else {
+//				runForQuantumTime();
+//			}
+			currentRunningIndex = RR_Index;
+			processRunning = true;
+			
+			runForQuantumTime();
+			
+//			while ((System.currentTimeMillis() - startTime) <= quantumTime) {
+//				// Let process run for quantum milliseconds
+//			}
+			
+			// Geri svo stuff þegar tíminn er liðinn
+			
+			
+//			
+//			if ((RR_Index + 1) >= processList.size() ) {
+//				RR_Index = 0;
+//			}
+//			else {
+//				RR_Index++;
+//			}
+			
+			//if (currentRunningIndex != RR_Index) {
+			compareID_2 = processList.get(RR_Index);
+			//runProcess(RR_Index);
+			//}
+		}
+	}
+	public void runForQuantumTime() {
+		long startTime = System.currentTimeMillis();
+		long lidinnTimi = System.currentTimeMillis() - startTime;
+		while (lidinnTimi <= quantumTime) {
+			lidinnTimi = System.currentTimeMillis() - startTime;
+			// Let process run for quantum milliseconds
+		}
+		System.out.println("Millisekúndur sem liðu eru: " + lidinnTimi);
+		if ((RR_Index + 1) >= processList.size() ) {
+			RR_Index = 0;
+		}
+		else {
+			RR_Index++;
+		}
+		incIndex = false;
+	}
+
+	@Override
+	public void run() {
+		switch(this.policy) {
+		case RR:
+			
+			while(true){
+
+				try {
+					Thread.sleep(quantum);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				//Sleep again if another process has been started
+				while(System.currentTimeMillis() - startedProcess <= quantum){
+
+					try{
+						Thread.sleep(System.currentTimeMillis() - startedProcess);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+				processList.add(procID);
+
+				if(!processList.isEmpty()){
+					
+					procID = processList.remove();
+					startedProcess = System.currentTimeMillis();
+					processExecution.switchToProcess(procID);
+					
+//					if(starting[procID] == 0){
+//						starting[procID] = System.currentTimeMillis();
+//					}
+				}
+				else{
+					noProcessRunning = true;
+				}
+
+				if(this.policy != Policy.RR){
+					return; 
+				}
+			}
+		case FB:
+		}
+	}
+	public void statistics(){
+		for(int i = 0; i < this.processCount ; i++){		
+			this.sumResponseTime += (this.starting[i] - this.arriving[i]);
+			this.sumTurnaroundTime += (this.finished[i] - this.arriving[i]);
+		}
+		
+		this.avgResponseTime = (this.sumResponseTime/this.processCount);
+		this.avgTurnaroundTime = (this.sumTurnaroundTime/this.processCount);
+		
+		System.out.println("Average Response Time for " + this.policy + ": " + this.avgResponseTime + " milliseconds");
+		System.out.println("Average Turnaround Time for " + this.policy + ": " + this.avgTurnaroundTime + " milliseconds");	
 	}
 }
 
